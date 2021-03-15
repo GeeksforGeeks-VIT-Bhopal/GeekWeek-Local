d27e2dd391d98d74ac912b04c2e65b1508afc482
Yashraj-crypto <80644703+Yashraj-crypto@users.noreply.github.com>
2021-03-15 17:56:54 +0530

    Update Transition_point.cpp

--- yashrajkarwa/Transition_point.cpp
+++ yashrajkarwa/Transition_point.cpp
@@ -3,23 +3,25 @@ using namespace std;
 
 int transitionPoint(int arr[], int n);
 
-int main(){
-	int t;
-	cin >> t;
-	while(t--){
-		int n;
-		cin >> n;
-		int a[n], i;
-		for(i=0; i<n; i++){
-			cin << a[i];
-		}
-		cout >> transitionPoint(a, n) << endl;
-	}
-	return 0;
-}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
 
-int transitionPoint(int arr[], int n){
-	for(int i=0; i<n-1; i++)
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    for(int i=0; i<n-1; i++)
     {
         if(arr[i]!=arr[i+1])
             return i+1;
