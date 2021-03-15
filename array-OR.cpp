6bcafb4fc8c8e6f34441a9dec358d70c0d06b2e1
adarsh-jha16 <80684400+adarsh-jha16@users.noreply.github.com>
2021-03-15 16:51:27 +0530

    Add files via upload

---
+++ array-OR.cpp
@@ -0,0 +1,43 @@
+// https://practice.geeksforgeeks.org/problems/play-with-or5515/1/?category[]=Arrays&category[]=Arrays&page=1&query=category[]Arrayspage1category[]Arrays#
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int *game_with_number(int arr[], int n);
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        int *arr2;
+
+        arr2 = game_with_number(arr, n);
+        for (int i = 0; i < n; i++)
+            cout << arr2[i] << " ";
+
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+int *game_with_number(int arr[], int n)
+{
+
+    // Complete the function
+    for (int i = 0; i < n - 1; i++)
+    {
+        arr[i] = arr[i] | arr[i + 1];
+    }
+    return arr;
+}
