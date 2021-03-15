443b4cefbf1e5ff8b96e13c7ac79da59b393bb91
Hk0509 <80637998+Hk0509@users.noreply.github.com>
2021-03-15 17:25:56 +0530

    Cpp file by Himanshu

---
+++ Himanshu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    
+    cout << "Enter two integers: ";
+    cin >> a >> br;
+
+    // sum of two numbers in stored in variable c
+    c = a + b;
+
+    // Prints sum 
+    cout << a << " + " <<  b << " = " << c;     
+
+    return 0;
+}
