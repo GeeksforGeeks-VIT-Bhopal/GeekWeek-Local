6de765f602009ab5dac3aa271946b7adfe1895b0
Manikiran Peketi <80636388+ManikiranPeketi@users.noreply.github.com>
2021-03-16 18:31:17 +0530

    Update Day 2 dsa problem 1 .py

--- Day 2 dsa problem 1 .py
+++ Day 2 dsa problem 1 .py
@@ -1,10 +1,14 @@
-import string
-
-
-def ispangram(str):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    for char in alphabet:
-        if char not in str.lower():
-            return False
-
-    return True
+n=int(input())
+s=input()
+ans=[]
+c=''
+for i in range(0,n):
+    c = s[i]
+    if c>='A' and c<='Z':
+        c=chr(ord(c)+32)
+    if c not in ans:
+        ans.append(c)
+if int(len(ans)) == 26:
+    print("YES")
+else:
+    print("NO")
