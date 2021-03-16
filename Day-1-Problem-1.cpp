0faa67114da9a89483168301a7554c0c69fe9db9
Shreyash <75601500+Shreyashmohadikar@users.noreply.github.com>
2021-03-17 01:27:56 +0530

    Update Day-1-Problem-1.cpp

--- Day-1-Problem-1.cpp
+++ Day-1-Problem-1.cpp
@@ -1,11 +1,37 @@
-s=input()
-x=s.find('AB')
-y=s.find('BA',x+2)
-i=s.find('BA')
-j=s.find('AB',i+2)
-if x>-1 and y>-1:
-    print('YES')
-elif i>-1 and j>-1:
-    print('YES')
-else:
-    print('NO')
+
+#include <cstdlib>
+#include <memory.h>
+
+int S1[256], S2[256], S[256];
+
+void answer(string s1, string s2, string s)
+{
+    memset(S1, 0, sizeof S1);
+    memset(S2, 0, sizeof S2);
+    memset(S, 0, sizeof S);
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        S1[s1[i]]++;
+    }
+
+    for (int i = 0; i < s2.size(); i++)
+    {
+        S2[s2[i]]++;
+    }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        S[s[i]]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (S[i] != S1[i] + S2[i])
+        {
+            cout << "NO" << endl;
+            exit(0);
+        }
+    }
+    cout << "YES" << endl;
+    return;
+}
