01329669629061375bbe0ef8a9329660cdf5cb5a
Rinal Shankar <80646439+rinal-2708@users.noreply.github.com>
2021-03-17 00:34:23 +0530

    Create Day-3-Problem-3.CPP

---
+++ rinal-2708/Day-3-Problem-3.CPP
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    char* a[10] = {"Greater than 9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    scanf("%d", &n);
+
+    if(n > 9) 
+       puts(a[0]);
+  
+    else 
+        puts(a[n]);
+    
+
+    return 0;
+
+}
