bbe0b9ea33103cc589c4c4fc6118e7f2a1c230ed
Mudit Sharma <80636595+mudit-02@users.noreply.github.com>
2021-03-16 18:39:00 +0530

    Create Day 2_problem1.py

---
+++ mudit-02/Day 2_problem1.py
@@ -0,0 +1,13 @@
+a=int(input("Enter a"))
+b=float(input("Enter b"))
+c=int(input("Enter c"))
+if (a>b):
+    if (a>c):
+        print("A is the greatest")
+    else:
+        print("C is greatest")
+if (b>a):
+    if(b>c):
+        print("B is the greatest")
+    else:
+        print("C is the greatest")
