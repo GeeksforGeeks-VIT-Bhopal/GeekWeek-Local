1aef9c5dc16f2dfa792dca3bc672762c83d5e570
IshikaBarak4220 <80664396+IshikaBarak4220@users.noreply.github.com>
2021-03-16 16:38:04 +0530

    Add files via upload

---
+++ IshikaBarak4220/geek1.py
@@ -0,0 +1,13 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: D:/Ishika/2nd SEM/cs/1.py ======================
+Initial String: 
+GeeksForGeeks
+
+First character of String is: 
+G
+
+Last character of String is: 
+s
+>>> 
