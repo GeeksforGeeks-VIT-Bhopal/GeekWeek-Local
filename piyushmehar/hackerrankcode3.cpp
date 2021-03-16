92100ec9bcc9571bb858d8257b0b728c09ee73da
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2021-03-16 23:29:22 +0530

    Fraudulent Activity Notifications IN CPP

--- piyushmehar/hackerrankcode3.cpp
+++ piyushmehar/hackerrankcode3.cpp
@@ -1,75 +1,50 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-vector<string> split_string(string);
-
-// Complete the activityNotifications function below.
-int activityNotifications(vector<int> expenditure, int d) {
-
-
+int arr[200010];
+int temp_arr[200010];
+int counts[210];
+float get_median(int size) {
+    int index = 0;
+    for (int i = 0; i < 210 ; i++) {
+        int count = counts[i];
+        while (count--) {
+            temp_arr[index++] = i;
+            if (index > size/2) { break; }
+        }
+        if (index > size/2) { break; }
+    }
+    return size % 2 == 1 ? temp_arr[size/2] : (temp_arr[size/2] + temp_arr[size/2-1])/2.0;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+int main() {
+    int n, d;
 
-    string nd_temp;
-    getline(cin, nd_temp);
-
-    vector<string> nd = split_string(nd_temp);
-
-    int n = stoi(nd[0]);
-
-    int d = stoi(nd[1]);
-
-    string expenditure_temp_temp;
-    getline(cin, expenditure_temp_temp);
-
-    vector<string> expenditure_temp = split_string(expenditure_temp_temp);
-
-    vector<int> expenditure(n);
-
-    for (int i = 0; i < n; i++) {
-        int expenditure_item = stoi(expenditure_temp[i]);
-
-        expenditure[i] = expenditure_item;
+    scanf("%d %d", &n, &d);
+    for (int i = 0; i < n ; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    int result = activityNotifications(expenditure, d);
+    int alarm_count = 0;
+    float median = 210;
 
-    fout << result << "\n";
+    for (int i = 0; i < d ; i++) {
+        counts[arr[i]]++;
+    }
+    median = get_median(d);
 
-    fout.close();
+    for (int i = d; i < n; i++) {
+        if (arr[i] >= 2*median) {
+            alarm_count++;
+        }
 
+        // update counts array
+        counts[arr[i-d]]--;
+        counts[arr[i]]++;
+
+        median = get_median(d);
+    }
+
+    printf("%d\n", alarm_count);
     return 0;
 }
 
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
-}
