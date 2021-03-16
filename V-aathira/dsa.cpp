4e411a78ea75369a5ba1f89d35125806cbe62be9
Aathira Varma <66830820+V-aathira@users.noreply.github.com>
2021-03-16 17:52:16 +0530

    Create dsa.cpp

---
+++ V-aathira/dsa.cpp
@@ -0,0 +1,18 @@
+//Removing consecutive duplicates
+
+string removeConsecutiveDuplicates(string s)
+{
+    if (s.size() <= 1)
+    {
+        return s;
+    }
+    if (s[0] == s[1])
+    {
+        return removeConsecutiveDuplicates(s.substr(1));
+    }
+    else
+    {
+        string small = removeConsecutiveDuplicates(s.substr(1));
+        return s[0] + small;
+    }
+}
