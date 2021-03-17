93bbaa03c7968ec425b9ead1b777d1fb248ab656
ananyaagra <64300750+ananyaagra@users.noreply.github.com>
2021-03-17 17:51:34 +0530

    created by ananya

---
+++ ananyaagra/d3programme1.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    if (year%4)==0:
+        if(year%100) == 0:
+            if(year%400)==0:
+                return True
+            else: return False
+        else: return True
+    else: return False
+
+year = int(input())
+print(is_leap(year))
