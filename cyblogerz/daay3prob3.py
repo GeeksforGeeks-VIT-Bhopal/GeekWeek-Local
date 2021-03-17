e0d51a060956463d54113e3314af8a99c134bb6b
Pranav Ajay <pranavajay2002@gmail.com>
2021-03-17 19:32:05 +0530

    DSA problems solved

---
+++ cyblogerz/daay3prob3.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, nlist):
+    # Write your code here
+    for i in range(d):
+        nlist.append(nlist[0])
+        nlist.remove(nlist[0])
+    return nlist
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
