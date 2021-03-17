70b1e07a772eae9575f23be320974cae82767c4d
Shreyass2 <shreyasrajak@gmail.com>
2021-03-17 19:17:48 +0530

    gfg challenge

--- shreyas.py
+++ shreyas.py
@@ -1,17 +1,16 @@
-# Python Program to find the area of triangle
-
-a = 5
-b = 6
-c = 7
-
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
-
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+#Shreyas Rajak 20MEI10005
+def numvowel(ch):
+    return ch.upper() in ['A','E','I','O','U']
+def count(str):
+    count1 = 0
+    count2 = 0
+    for i in range (len(str)):
+        if numvowel(str[i]):
+            count1 += 1
+        else:
+            count2 += 1
+        return count1 , count2
+str = input("enter the string: ")
+a = count(str)
+print(a)
+print("number of vowels,number of consonants")
