3bb9c10f419ef9752a765568033ab84becac2243
Varun Jain <80705703+VarunJain-2001@users.noreply.github.com>
2021-03-18 11:10:20 +0530

    Create day-4-problem-3.py

---
+++ day-4-problem-3.py
@@ -0,0 +1,38 @@
+INT_MAX = 4294967296
+INT_MIN = -4294967296
+
+class Node:
+
+	def __init__(self, data):
+		self.data = data 
+		self.left = None
+		self.right = None
+
+def isBST(node):
+	return (isBSTUtil(node, INT_MIN, INT_MAX))
+
+def isBSTUtil(node, mini, maxi):
+	
+
+	if node is None:
+		return True
+
+
+	if node.data < mini or node.data > maxi:
+		return False
+
+	return (isBSTUtil(node.left, mini, node.data -1) and
+		isBSTUtil(node.right, node.data+1, maxi))
+
+
+root = Node(4)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(1)
+root.left.right = Node(3)
+
+if (isBST(root)):
+	print "True"
+else:
+	print "False"
+
