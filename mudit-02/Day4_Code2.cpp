9b4ffbd288ead4a805816e7c2e12f9a57741b6a8
Mudit Sharma <80636595+mudit-02@users.noreply.github.com>
2021-03-18 16:13:53 +0530

    Create Day4_Code2.cpp

---
+++ mudit-02/Day4_Code2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+main() {
+   int max;
+   max = 100; //set the upper bound to generate the random number
+   srand(time(0));
+   cout << "The random number is: "<<rand()%max;
+}
