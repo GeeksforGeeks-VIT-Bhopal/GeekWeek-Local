e5460a5a6c053730805a89249b01842d4e500c41
Akshra <63922909+akshras@users.noreply.github.com>
2021-03-18 15:48:30 +0530

    Create N-Queens.cpp

---
+++ N-Queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int n;
+    
+    void backtrack(vector<bool>& used_col, vector<bool>& used_pos_diag, 
+                   vector<bool>& used_neg_diag, int r, 
+                   vector<int>& curpos, vector<vector<string>>& ans){
+        if(r == n){
+            vector<string> curans;
+            for(int i = 0; i < n; ++i){
+                string row(n, '.');
+                row[curpos[i]] = 'Q';
+                curans.push_back(row);
+            }
+            ans.push_back(curans);
+        }else{
+            for(int c = 0; c < n; ++c){
+                if(!used_col[c] && !used_pos_diag[r-c+n-1] && !used_neg_diag[r+c]){
+                    curpos.push_back(c);
+                    used_col[c] = true;
+                    used_pos_diag[r-c+n-1] = true;
+                    used_neg_diag[r+c] = true;
+                    
+                    backtrack(used_col, used_pos_diag, used_neg_diag, r+1, curpos, ans);
+                    
+                    used_col[c] = false;
+                    used_pos_diag[r-c+n-1] = false;
+                    used_neg_diag[r+c] = false;
+                    curpos.pop_back();
+                }
+            }
+        }
+    };
+    
+    vector<vector<string>> solveNQueens(int n) {
+        this->n = n;
+        vector<int> curpos;
+        vector<vector<string>> ans;
+        //in each recursion, r is different, so there must be only one Q in a row
+        //for each row, need to check if that column is used
+        vector<bool> used_col(n, false);
+        //also check if that positive diagonals is used
+        //there are 2*n-1 positive diagonals
+        vector<bool> used_pos_diag(2*n-1, false);
+        //also check if that positive diagonals is used
+        //there are 2*n-1 negative diagonals
+        vector<bool> used_neg_diag(2*n-1, false);
+        
+        backtrack(used_col, used_pos_diag, used_neg_diag, 0, curpos, ans);
+        
+        return ans;
+    }
+};
