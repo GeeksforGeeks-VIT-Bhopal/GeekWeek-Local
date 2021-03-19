54285bc0cdac27a3e5dde617c9d742b0685fb4fc
Divashree-27 <80639950+Divashree-27@users.noreply.github.com>
2021-03-19 17:43:30 +0530

    Create Day5 Program1.py

---
+++ Divashree-27/Day5 Program1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        len_ = 1
+        if len(nums)==0:
+            return 0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[len_] = nums[i]
+                len_ +=1
+        return len_
