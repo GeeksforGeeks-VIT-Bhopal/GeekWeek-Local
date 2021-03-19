ef7298d98f1ad1e0104f001389e370d8cf4be509
646divi <80637738+646divi@users.noreply.github.com>
2021-03-19 18:12:55 +0530

    Create topcoder_3.cpp

---
+++ topcoder_3.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define sz(x) (int)(x.size())
+#define mod 1000000007
+#define endl "\n"
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+class MaximumBalances{
+public:
+    int solve(string s){
+        int o=0, c=0;
+        for(auto g:s){
+            if(g=='('){
+                o++;
+            }
+            else{
+                c++;
+            }
+        }
+        int poss=min(o,c);
+        int ans=0;
+        // ans+=poss;
+        for(int i=0;i<poss;i++){
+            for(int j=i;j<poss;j++){
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    MaximumBalances M;
+    string s;
+    cin>>s;
+    deb(M.solve(s));
+
+    return 0;
+}
