eb58208dfdf1da5c4f6d2bb3fecad349431bed92
Aditya Patil <75464869+Aditya1402@users.noreply.github.com>
2021-03-20 00:45:13 +0530

    Create code 2.cpp

---
+++ code 2.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <climits>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a[6][6],s;
+    int m=INT_MIN;
+    //int s;
+    for(int i=0;i<6;i++)
+        {
+        for(int j=0;j<6;j++)
+            {
+            cin>>a[i][j];
+        }
+    }
+  // int j;
+    for(int i=0;i<4;i++)
+        {
+         // j=0;
+        for(int j=0;j<4;j++)
+            {
+            s=(a[i][j]+a[i][j+1]+a[i][j+2]+a[i+1][j+1]+a[i+2][j]+a[i+2][j+1]+a[i+2][j+2]);
+            if(s>m)
+                m=s;
+        }
+
+
+            }
+    cout<<m;
+
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
