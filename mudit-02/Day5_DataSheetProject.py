55257091ebcea11df3e1b5bb179fa475533668e0
Mudit Sharma <80636595+mudit-02@users.noreply.github.com>
2021-03-19 12:00:34 +0530

    Create Day5_DataSheetProject.py

---
+++ mudit-02/Day5_DataSheetProject.py
@@ -0,0 +1,13 @@
+#DataSheet Project
+
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+df= pd.read_csv('titanic.csv')
+
+print(df)
+
+#Generate a report
+profile = ProfileReport(df, minimal=True)
+profile.to_file(output_file='titanic.html')
+
