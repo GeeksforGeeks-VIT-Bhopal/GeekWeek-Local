f39500e22d4d9d870bce67969775d1a38815fa6d
Mudit Sharma <80636595+mudit-02@users.noreply.github.com>
2021-03-20 16:16:13 +0530

    Create Day6_DSA2.py

---
+++ mudit-02/Day6_DSA2.py
@@ -0,0 +1,16 @@
+//LEETCODE DSA
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
