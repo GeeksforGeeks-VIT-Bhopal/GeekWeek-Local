cf62992ccc1476d988267a95e3c56d8266412df2
Paritosh Nimdeo <80650433+Paritosh0-0@users.noreply.github.com>
2021-03-20 23:43:07 +0530

    Day 6 DSA 3

--- DSA 3.py
+++ DSA 3.py
@@ -1,10 +1,15 @@
-class ABBA:
-    def canObtain(self, initial, target):
-        if initial == target:
-            return 'Possible'
-        if len(initial) == len(target):
-            return 'Impossible'
-        if target[-1] == 'A':
-            return self.canObtain(initial, target[:-1])
-        if target[-1] == 'B':
-            return self.canObtain(initial, target[:-1][::-1])
+s = input()
+a = 1
+t = tuple()
+l = []
+for i in range(len(s)):    
+    if i == len(s)-1:
+        l.append((a,int(s[i])))
+        break
+    if s[i] == s[i+1]:
+        a += 1
+    else:
+        l.append((a,int(s[i])))
+        a = 1
+for j in l:
+    print(j, end = " ")
