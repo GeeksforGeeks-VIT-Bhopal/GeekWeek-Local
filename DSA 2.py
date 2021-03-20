374832713ad46ba5ea2cc3b2ec4d90c4c52dd783
Paritosh Nimdeo <80650433+Paritosh0-0@users.noreply.github.com>
2021-03-20 23:42:53 +0530

    Day 6 DSA 2

--- DSA 2.py
+++ DSA 2.py
@@ -1,19 +1,21 @@
-class AB:
-    def createString(self, N, K):
-        places_max = int(N / 2)
-        if N % 2:
-            places_max += 1
-        places_list = (N - i for i in range(places_max))
-        letters = list('A' * N)
-        pairs = 0
-        for places in places_list:
-            for i in range(1, places):
-                if pairs == K:
-                    return ''.join(letters)
-                letters[i - 1] = 'A' 
-                letters[i] = 'B'  
-                pairs += 1
-            if pairs == K:
-                return ''.join(letters)
-            pairs -= 1  
-        return ''
+def minion_game(string):
+    s, k = 0, 0
+    l = list(string)
+    x = len(l)
+    for w in l:
+        if w in "aeiouAEIOU":
+            k = k + x
+        else:
+            s = s + x
+        x = x - 1
+
+    if s > k:
+        print ('Stuart', s)
+    elif k > s:
+        print ('Kevin', k)
+    else:
+        print ('Draw')
+                
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
