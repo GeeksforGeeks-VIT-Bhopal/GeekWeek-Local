16b4035660f1b4fe07dff1a614a3b3cef819fe82
Paritosh Nimdeo <80650433+Paritosh0-0@users.noreply.github.com>
2021-03-20 23:42:33 +0530

    Day 6 DSA 1

--- DSA 1.py
+++ DSA 1.py
@@ -1,10 +1,4 @@
-class ABBA:
-    def canObtain(self, initial, target):
-        if initial == target:
-            return 'Possible'
-        if len(initial) == len(target):
-            return 'Impossible'
-        if target[-1] == 'A':
-            return self.canObtain(initial, target[:-1])
-        if target[-1] == 'B':
-            return self.canObtain(initial, target[:-1][::-1])
+from datetime import datetime as dt
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(), fmt) - dt.strptime(input(), fmt)).total_seconds())))
