72d264e879847921d2202a141587105b83207be1
Prakshi Agrawal <80665069+prakshi18@users.noreply.github.com>
2021-03-20 23:58:12 +0530

    Create day6probl5.py

---
+++ prakshi18/day6probl5.py
@@ -0,0 +1,6 @@
+from collections import Counter, OrderedDict
+class OrderedCounter(Counter, OrderedDict):
+    pass
+d = OrderedCounter(input() for _ in range(int(input())))
+print(len(d))
+print(*d.values())
