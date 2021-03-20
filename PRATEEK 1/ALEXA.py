c1beb966fca3cb99edf15f28d7d565d595933301
supercoder246 <76002554+supercoder246@users.noreply.github.com>
2021-03-20 15:38:04 +0530

    my voice assistant

---
+++ PRATEEK 1/ALEXA.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_goo
