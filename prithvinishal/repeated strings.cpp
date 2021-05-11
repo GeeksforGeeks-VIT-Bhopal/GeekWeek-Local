4871b7549208a55eed263dd72a8fb50cac1027c6
prithvinishal <80667999+prithvinishal@users.noreply.github.com>
2021-05-11 23:07:36 +0530

    GWL Challenges By prithvinishal (#526)
    * readme by prithvinishal
    
    * new language code by prithvi nishal
    
    * Add files via upload

---
+++ prithvinishal/repeated strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    long long n,cnt = 0,rem,rept,remCnt = 0;
+    cin >> str;
+    cin >> n;
+    long long len = str.length();
+    rem = n%len;
+    rept = n/len;
+    for(int i = 0; i < len; i++){
+        if(str[i] == 'a')cnt++;
+        if(str[i] == 'a' && i < rem)remCnt++;
+    }
+    cout << (cnt*rept)+remCnt << endl;
+    return 0;
+}
